0e2496f944ed13fa1609bb8a0fd8951ec29722bb
Youzhi Yu <yuyouzhi666@icloud.com>
2021-06-09 15:59:37 -0400

    added two plots

--- 2021-06-08/2020-06-08.R
+++ 2021-06-08/2020-06-08.R
@@ -68,3 +68,43 @@ stocked %>% select(year, lake, species, no_stocked, weight) %>%
   ggtitle("Time-Wise Fishes Stocked In The Great Lakes")
 
 #ggsave("Time-Wise Fishes Stocked In The Great Lakes.png", height = 10, width = 20)
+
+#ggstream does not work for this code
+# library(ggstream)
+# stocked %>% select(year, lake, species, no_stocked, weight) %>%
+#   mutate(lake = case_when(
+#     lake == "MI" ~ "Michigan",
+#     lake == "SU" ~ "Superior",
+#     lake == "ON" ~ "Ontario",
+#     lake == "ER" ~ "Erie",
+#     lake == "HU" ~ "Huron",
+#     lake == "SC" ~ "Saint Clair"
+#   )) %>%
+#   drop_na() %>%
+#   ggplot(aes(year, weight, fill = species)) +
+#   geom_stream()
+
+fishing %>% select(-comments) %>%
+  filter(species %in% top_10_fishes$species) %>%
+  group_by(lake, species, year) %>%
+  summarize(`total number of fish`= sum(values)) %>%
+  arrange(desc(`total number of fish`)) %>%
+  ggplot(aes(year, log(`total number of fish`), fill = species)) +
+  geom_violin() +
+  facet_grid(lake~species, scales = "free") +
+  theme_bw() +
+  theme(
+    strip.text = element_text(size=15, face = "bold"),
+    axis.ticks = element_blank(),
+    axis.title = element_text(size = 15),
+    axis.text.y = element_text(size = 15),
+    axis.text.x = element_text(size = 15, angle = 30),
+    legend.position = "bottom",
+    legend.text = element_text(size = 15),
+    legend.title = element_blank(),
+    plot.title = element_text(size = 16)
+  ) +
+  scale_x_continuous(breaks = seq(1900, 2018, 50)) + 
+  labs(x = "year", y = "log-scale total number of fish") +
+  ggtitle("Time Series 10 Most Populated Fishes In The Great Lakes")
+#ggsave("Time Series 10 Most Populated Fishes In The Great Lakes.png", height = 10, width = 20)
