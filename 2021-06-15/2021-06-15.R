fc5db460b6e990a99b7b2f5cceb5e1f2544ed242
Youzhi Yu <yuyouzhi666@icloud.com>
2021-06-17 09:01:11 -0400

    added sentiment analysis

--- 2021-06-15/2021-06-15.R
+++ 2021-06-15/2021-06-15.R
@@ -46,7 +46,10 @@ tweets %>% group_by(weekofday) %>%
     axis.title.y = element_blank(),
     plot.title = element_text(size = 16)
   ) +
-  coord_flip()
+  coord_flip() +
+  ggtitle("Tweets Distribution Across The Week")
+
+#ggsave("Tweets Distribution Across The Week.png", width = 20, height = 10)
 
 
 tweets %>% group_by(username) %>%
@@ -66,7 +69,12 @@ tweets %>% group_by(username) %>%
     axis.text.x = element_text(size = 10),
     legend.position = "bottom",
     plot.title = element_text(size = 16)
-  )
+  ) +
+  ggtitle("Top 10 Tweet Users On #DuBoisChallenge")
+
+#ggsave("Top 10 Tweet Users On #DuBoisChallenge.png", width = 20, height = 10)
+
+
 library(tidytext)
 
 tweets%>% select(content) %>%
@@ -92,9 +100,84 @@ tweets%>% select(content) %>%
 get_sentiments("nrc") %>% 
   filter(sentiment == "joy")
 
+tweets %>% select(content)%>% 
+  mutate(linenumber = row_number())
+
+tweets%>% select(content) %>%
+  mutate(linenumber = row_number()) %>%
+  unnest_tokens(word, content) %>%
+  anti_join(stop_words) %>%
+  #count(word, sort = TRUE) %>%
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(index = linenumber) %>% 
+  summarise(sentiment = sum(value)) %>% 
+  mutate(method = "AFINN") %>%
+  ggplot(aes(index, sentiment, fill = ifelse(sentiment < 0, "red", "blue"))) +
+    geom_col(show.legend = FALSE, width = 2) +
+  labs(x = "tweet", title = "Tweet Sentiment Distribution (AFINN)") +
+  theme_bw() +
+  theme(
+    axis.ticks = element_blank(),
+    axis.title = element_text(size = 15),
+    axis.text.y = element_text(size = 10),
+    axis.text.x = element_text(size = 10),
+    legend.position = "bottom",
+    plot.title = element_text(size = 16)
+  )
 
 
+afinn <- tweets%>% select(content) %>%
+  mutate(linenumber = row_number()) %>%
+  unnest_tokens(word, content) %>%
+  anti_join(stop_words) %>%
+  #count(word, sort = TRUE) %>%
+  inner_join(get_sentiments("afinn")) %>% 
+  group_by(linenumber) %>% 
+  summarise(sentiment = sum(value)) %>% 
+  mutate(method = "AFINN") 
+
+bing_and_nrc  <- bind_rows(
+tweets%>% select(content) %>%
+  mutate(linenumber = row_number()) %>%
+  unnest_tokens(word, content) %>%
+  anti_join(stop_words) %>%
+  inner_join(get_sentiments("bing")) %>%
+  mutate(method = "Bing et al."),
+tweets%>% select(content) %>%
+  mutate(linenumber = row_number()) %>%
+  unnest_tokens(word, content) %>%
+  anti_join(stop_words) %>% 
+  inner_join(get_sentiments("nrc")) %>% 
+               filter(sentiment %in% c("positive", 
+                                       "negative"))%>%
+               mutate(method = "NRC")) %>%
+  count(method, linenumber, sentiment) %>%
+  pivot_wider(names_from = sentiment,
+              values_from = n,
+              values_fill = 0) 
+
+bing_and_nrc <- bing_and_nrc %>% 
+  mutate(sentiment = positive - negative)
+
+bing_and_nrc <- bing_and_nrc %>% select(linenumber, sentiment, method)
 
 
+bind_rows(afinn, 
+          bing_and_nrc) %>%
+  ggplot(aes(linenumber, sentiment, fill = method)) +
+  geom_col(show.legend = FALSE, width = 1) +
+  facet_wrap(~method, ncol = 1, scales = "free_y") +
+  labs(x = "tweet", title = "Tweets Sentiment Distribution") +
+  theme_bw() +
+  theme(
+    strip.text = element_text(size = 15),
+    axis.ticks = element_blank(),
+    axis.title = element_text(size = 15),
+    axis.text.y = element_text(size = 10),
+    axis.text.x = element_text(size = 10),
+    legend.position = "bottom",
+    plot.title = element_text(size = 16)
+  )
 
-
+#ggsave("Sentiment Analysis.png", width = 20, height = 10)
+             
