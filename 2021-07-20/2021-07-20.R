9a05e3caab38e25cd9eb1941456121a8605ee27d
Youzhi Yu <yuyouzhi666@icloud.com>
2021-07-21 09:57:56 -0400

    added new plot

--- 2021-07-20/2021-07-20.R
+++ 2021-07-20/2021-07-20.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(lubridate)
 library(patchwork)
+library(geofacet)
 drought <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2021/2021-07-20/drought.csv')
 stateabb <- read_csv("stateabb.csv")
 
@@ -15,6 +16,7 @@ drought <- drought %>%
 states_map <- map_data("state")
 stateabb <- stateabb %>% select(State, Code)
 
+# The first plot -----
 
 map_fun <- function(cal_year){
 drought %>% filter(year == cal_year, month %in% c(5,6,7)) %>%
@@ -43,3 +45,25 @@ drought %>% filter(year == cal_year, month %in% c(5,6,7)) %>%
 map_fun(2018) + map_fun(2019) + map_fun(2020) + map_fun(2021) + plot_layout(nrow = 2, byrow = FALSE)
 
 #ggsave("Average Population Affected.png", width = 20, height = 15)
+
+# The second plot -----
+
+drought %>% filter(drought_lvl != 'None') %>%
+  group_by(state_abb, year, drought_lvl) %>%
+  summarize(mean(area_pct)) %>%
+  ggplot(aes(year, `mean(area_pct)`, color = drought_lvl)) +
+  geom_line() +
+  geom_point() +
+  facet_geo(~state_abb) +
+  theme_bw() +
+  theme(
+    strip.text = element_text(face = "bold"),
+    plot.title = element_text(size = 18),
+    axis.title = element_text(size = 15),
+    axis.text = element_text(size = 10, angle = 25)
+  ) +
+  labs(y = "Average Area Affected (%)", title = "State-wise Average Area Affected By Drought (2000 - 2020)")
+  
+#ggsave("State-wise Average Area Affected By Drought (2000 - 2020).png", width = 20, height = 10) 
+  
+
