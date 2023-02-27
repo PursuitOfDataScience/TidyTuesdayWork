0682be9da036edb8e4109aab0866bb99ccbb6724
Youzhi Yu <yuyouzhi666@icloud.com>
2023-02-27 17:00:48 -0600

    added new week data

---
+++ 2023-02-28/2023-02-18.R
@@ -0,0 +1,29 @@
+library(tidyverse)
+theme_set(theme_light())
+
+afrisenti <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-28/afrisenti.csv')
+languages <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-28/languages.csv')
+language_scripts <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-28/language_scripts.csv')
+language_countries <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-28/language_countries.csv')
+country_regions <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-28/country_regions.csv')
+
+
+# join afrisenti and language_countries, country_regions together
+
+afrisenti |>
+  #select(-tweet) |>
+  left_join(language_countries, by = "language_iso_code", multiple = "all") |>
+  distinct() |>
+  left_join(country_regions, by = "country", multiple = "all") |>
+  left_join(language_scripts, by = "language_iso_code", multiple = "all") |>
+  left_join(languages, by = "language_iso_code", multiple = "all")
+
+
+
+
+
+
+
+
+
+
